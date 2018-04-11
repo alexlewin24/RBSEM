abdd360f3a493b95c414464995c9cfd220efddf3
marcobanterle <marco.banterle@gmail.com>
2018-04-11 10:20:23 +0100

    M: added cmath constants definitions

--- rBSEM/src/distr.h
+++ rBSEM/src/distr.h
@@ -2,13 +2,16 @@
 #define DISTR
 
 #include <armadillo>
+
+#define _USE_MATH_DEFINES
 #include <cmath>
+
 #include <boost/math/special_functions/erf.hpp> // can I do this?
 #include <random>
 
-#ifndef M_PI
-  #define M_PI 3.14159265358979323846
-#endif
+//#ifndef M_PI
+//  #define M_PI 3.14159265358979323846
+//#endif
 
 
 namespace Distributions{
